5c9838cd2ddb5ab6d39633191db183c4f5271a32
Thomas Müller <thomas.mueller@kiprotect.com>
2023-05-03 14:42:12 +0200

    Add caching headers to file server.

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package gospel
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 	"strings"
 )
 
 type Server struct {
+	fs         fs.FS
 	server     *http.Server
 	fileServer http.Handler
 	app        *App
@@ -19,6 +25,41 @@ type PrefixFS struct {
 
 func (f *PrefixFS) Open(name string) (fs.File, error) {
 	return f.fs.Open(name[len(f.prefix):])
+}
+
+func etagCacheControl(h http.Handler, staticFs http.FileSystem) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		filePath := filepath.Join(".", r.URL.Path)
+
+		// Check if the file exists and is not a directory
+		file, err := staticFs.Open(filePath)
+		if err == nil {
+			defer file.Close()
+			fileInfo, err := file.Stat()
+			if err == nil && !fileInfo.IsDir() {
+				// Read file contents to compute the ETag
+				fileContents, err := ioutil.ReadAll(file)
+				if err == nil {
+					etag := computeETag(fileContents)
+					w.Header().Set("ETag", etag)
+					w.Header().Set("Cache-Control", "private, no-store, must-revalidate")
+				}
+			}
+		}
+
+		// If the ETag in the request matches the computed ETag, return 304 Not Modified
+		if r.Header.Get("If-None-Match") == w.Header().Get("ETag") {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
+}
+
+func computeETag(data []byte) string {
+	hash := md5.Sum(data)
+	return fmt.Sprintf(`"%s"`, hex.EncodeToString(hash[:]))
 }
 
 type MultiFS struct {
@@ -37,9 +78,16 @@ func (m *MultiFS) Open(name string) (fs.File, error) {
 }
 
 func MakeServer(app *App) *Server {
+
+	fs := &PrefixFS{
+		fs:     &MultiFS{append([]fs.FS{JS}, app.StaticFiles...)},
+		prefix: app.StaticPrefix,
+	}
+
 	return &Server{
 		app:        app,
-		fileServer: http.FileServer(http.FS(&PrefixFS{fs: &MultiFS{append([]fs.FS{JS}, app.StaticFiles...)}, prefix: app.StaticPrefix})),
+		fs:         fs,
+		fileServer: http.FileServer(http.FS(fs)),
 		server: &http.Server{
 			Addr: ":8001",
 		},
@@ -51,6 +99,32 @@ var makeInMemoryStore = MakeInMemoryStoreRegistry()
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if strings.HasPrefix(r.URL.Path, s.app.StaticPrefix) {
+
+		filePath := filepath.Join(".", r.URL.Path)
+
+		// Check if the file exists and is not a directory
+		file, err := s.fs.Open(filePath)
+
+		if err == nil {
+			defer file.Close()
+			fileInfo, err := file.Stat()
+			if err == nil && !fileInfo.IsDir() {
+				// Read file contents to compute the ETag
+				fileContents, err := ioutil.ReadAll(file)
+				if err == nil {
+					etag := computeETag(fileContents)
+					w.Header().Set("ETag", etag)
+					w.Header().Set("Cache-Control", "private, max-age=0, must-revalidate")
+				}
+			}
+		}
+
+		// If the ETag in the request matches the computed ETag, return 304 Not Modified
+		if r.Header.Get("If-None-Match") == w.Header().Get("ETag") {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+
 		s.fileServer.ServeHTTP(w, r)
 		return
 	}
